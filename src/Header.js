338b51fa3c9d3c593e956b720b5c483d09d14f58
Chandrika <gehini1302@gmail.com>
2021-07-04 18:32:54 +0530

    Styled ‘Header’ (Inline Styling)

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,18 @@
  //functional component
  const Header =function(){
+    const headerStyle = {
+        textAlign: 'center',
+        padding: 20,
+        background: '#000',
+        color: '#fff',
+        textTransform: 'uppercase'
+     };
     return(
-    <div className="header">Phone-Directory</div>
+        <div>
+            <div style={headerStyle}>Phone-Directory</div>
+        </div>
+    
     )
  }
-// Class Component
-// class Header extends Component {
-//    render() {
-//        return (
-//            <div className="header">
-//                Phone Directory
-//            </div>
-//        )
-//    }
-// }
+
 export default Header;
