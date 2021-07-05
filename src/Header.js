54d2849ec8ba26fd5d33541768b2269ee5879a15
Chandrika <gehini1302@gmail.com>
2021-07-05 12:18:57 +0530

    Styled ‘Header’ (External Styling)

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,8 @@
- //functional component
+import './Header.css'
+//functional component
  const Header =function(){
-    const headerStyle = {
-        textAlign: 'center',
-        padding: 20,
-        background: '#000',
-        color: '#fff',
-        textTransform: 'uppercase'
-     };
     return(
-        <div>
-            <div style={headerStyle}>Phone-Directory</div>
-        </div>
-    
+    <div className="header">Phone Directory</div>
     )
  }
-
 export default Header;
