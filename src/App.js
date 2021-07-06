ea606b9a5ccc13314846003dccd36cac08da4266
Chandrika <gehini1302@gmail.com>
2021-07-06 21:39:33 +0530

    Added state in ‘AddSubscriber’ and ‘App’ components

--- src/App.js
+++ src/App.js
@@ -2,27 +2,31 @@ import React, { Component } from 'react';
 import Header from './Header.js';
 import './App.css';
 
+
+// let subscribers = [
+    //   {
+    //     id: 1,
+    //     name: "Shilpa Bhat",
+    //     phone: "8888888888"
+    //   },
+    //   {
+    //     id: 2,
+    //     name: "Srishti Gupta",
+    //     phone: "9999999999"
+    //   }
+    // ];
 class App extends Component {
-  clickHandler (message){
-    alert(message);
-    
+  constructor(){
+    super();
+    this.state={
+      subscribersListToShow: []
+    }
 
   }
+  
+  
 
   render() {
-
-    let subscribers = [
-      {
-        id: 1,
-        name: "Shilpa Bhat",
-        phone: "8888888888"
-      },
-      {
-        id: 2,
-        name: "Srishti Gupta",
-        phone: "9999999999"
-      }
-    ];
 
     return (
       <div>
@@ -36,7 +40,7 @@ class App extends Component {
           </div>
 
           {
-            subscribers.map(sub => {
+            this.state.subscribersListToShow.map(sub => {
               return <div key={sub.id} className="grid-container">
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
